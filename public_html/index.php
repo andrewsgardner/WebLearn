f532f100af93e29874e22d6450f1808ddf6a6610
Andrew S. Gardner <asg@andrewsgardner.com>
2016-05-02 09:16:21 -0400

    Added markup for landing page.

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,11 @@
-<?php echo "Testing... weblearn.dev"; ?>
+<?php require_once("config.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once(TEMPLATES_PATH . "/metadata.php"); ?>
+    </head>
+    <body>
+        
+    </body>
+</html>
