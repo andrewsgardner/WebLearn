a9ba307f79679b542f058936acc15c3ebcf27ee9
Andrew S. Gardner <asg@andrewsgardner.com>
2016-05-02 09:01:19 -0400

    Added config array and paths in config.php

---
+++ resources/config.php
@@ -0,0 +1,27 @@
+<?php
+$config = array(
+    "info" => array(
+        "siteName" => "WebLearn",
+        "cacheVer" => "?v=2.0"
+        ),
+    "paths" => array(
+        "styles" => array(
+            "css" => "./css"
+            ),
+        "img" => array(
+            "content" => "./img/content",
+            "layout" => "./img/layout"
+        )
+    ),
+    "jQuery" => array(
+        "cdn" => "https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js",
+        "local" => "./js/local/jquery-1.12.2.min.js"
+    )
+);
+
+defined("LIBRARY_PATH")
+	or define("LIBRARY_PATH", realpath(dirname(__FILE__) . "/library"));
+
+defined("TEMPLATES_PATH")
+	or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . "/templates"));
+?>
