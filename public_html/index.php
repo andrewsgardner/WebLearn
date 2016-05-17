36178a8d19655c37d94b1fd8e052a3e22ea60037
Andrew S. Gardner <asg@andrewsgardner.com>
2016-05-17 12:43:19 -0400

    Updated navbar markup.

--- public_html/index.php
+++ public_html/index.php
@@ -12,100 +12,64 @@ $meta = array(
         <?php require_once(TEMPLATES_PATH . "/metadata.php"); ?>
     </head>
     <body>
-        <!-- BEGIN header
-        <nav class="navbar navbar-fixed-top">
+    
+        <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        
-                            <span class="sr-only"></span>
-                        
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        
-                        <a class="pull-left" href="/"><img src="#"></a>
-                    </div>
-                    
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="/">Home<span class="sr-only">(current)</span></a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABE Lab <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Math</a></li>
-                                    <li><a href="#">Social Studies</a></li>
-                                    <li><a href="#">Technology</a></li>
-                                    <li><a href="#">English</a></li>
-                                    <li><a href="#">Science</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        
-                        <form class="navbar-form navbar-right" role="search">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
-                        
-                    </div>
+                
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/"><img src="/img/layout/weblearn-logo.png" alt="SALC WebLearn Logo"></a>
                 </div>
-            </div>
+
+                <div class="collapse navbar-collapse" id="primary-navbar">
+                    
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="/">Getting Started<span class="sr-only">(current)</span></a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABE Lab <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">English</a></li>
+                                <li><a href="#">Math</a></li>
+                                <li><a href="#">Science</a></li>
+                                <li><a href="#">Social Studies</a></li>
+                                <li><a href="#">Technology</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ESOL Lab <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Citizenship</a></li>
+                                <li><a href="#">General ESOL</a></li>
+                                <li><a href="#">Grammar</a></li>
+                                <li><a href="#">Health</a></li>
+                                <li><a href="#">MA Learner's Permit</a></li>
+                                <li><a href="#">Money</a></li>
+                                <li><a href="#">Parents</a></li>
+                                <li><a href="#">Pronunciation</a></li>
+                                <li><a href="#">Technology</a></li>
+                                <li><a href="#">Vocabulary</a></li>
+                                <li><a href="#">Workplace</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="col-md-3 col-sm-3 navbar-right">
+                    <form class="navbar-form navbar-right" role="search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                        </div>
+                    </div>
+                    </form>
+                    </div>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
         </nav>
-        END header -->
-
-            <nav class="navbar navbar-default navbar-fixed-top">
-                <div class="container-fluid">
-
-                    <div class="navbar-header">
-
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-
-                            <span class="sr-only"></span>
-
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        
-                        <a class="navbar-brand" href="/">SALC WebLearn</a>
-                    </div>
-                    
-                    <!-- change #bs-example-navbar-collapse-1 -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="/">Home <span class="sr-only">(current)</span></a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABE Lab <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <!-- <li role="separator" class="divider"></li> -->
-                                    <li><a href="#">English</a></li>
-                                    
-                                    <li><a href="#">Math</a></li>
-                                    
-                                    <li><a href="#">Science</a></li>
-                                    
-                                    <li><a href="#">Social Studies</a></li>
-                                    
-                                    <li><a href="#">Technology</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        
-                        <form class="navbar-form navbar-right" role="search">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-
         
         <?php require_once(TEMPLATES_PATH . "/loadScripts.php"); ?>
     </body>
