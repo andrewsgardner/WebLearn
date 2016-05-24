8beb8dbe056e799320c2161cae59378f41dad052
Andrew S. Gardner <asg@andrewsgardner.com>
2016-05-24 13:41:20 -0400

    Added abe dir.

---
+++ public_html/abe/index.php
@@ -0,0 +1,26 @@
+<?php 
+$currentPage = "abe";
+$pageTitle = "";
+$pageDesc = "";
+require_once("config.php");
+require_once(LIBRARY_PATH . "/contentPageFunctions.php");
+
+ob_start();
+?>
+
+<!-- BEGIN page content -->
+
+<h1>ABE Lab</h1>
+<p>Lorem ipsum dolor sit amet, pellentesque laoreet orci integer facilisi tortor quis, sollicitudin gravida torquent pellentesque proin, venenatis dolore cras iaculis wisi lacus egestas, wisi amet integer ultrices cursus irure est.</p>
+
+<!-- END page content -->
+
+<?php 
+$pageContent = ob_get_contents();
+ob_get_clean();
+
+$variables = array(
+    "pageContent" => $pageContent
+);
+renderContentPageLayout("/contentPage.php", $variables);
+?>
