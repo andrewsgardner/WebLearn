f689f5d85f030900e2317a102478a32b3808377c
Andrew S. Gardner <asg@andrewsgardner.com>
2016-06-03 08:51:26 -0400

    Added activityList.php template.

---
+++ resources/templates/activityList.php
@@ -0,0 +1,20 @@
+<?php
+
+/* BEGIN navbar-bottom */
+echo "\t<div class=\"navbar-bottom\">\n";
+echo "\t<div class=\"container\">\n";
+
+echo $pageBreadbrumbs;
+
+echo "\t</div>\n";
+echo "\t</div>\n";
+/* END navbar-bottom */
+
+/* BEGIN container */
+echo "\t<div class=\"container grid-container\">\n";
+
+echo $activityList;
+
+echo "\t</div>\n";
+/* END container */
+?>
