147bf75a4ccfa75d8c08af78f251e96fb9ef79ad
Andrew S. Gardner <asg@andrewsgardner.com>
2016-06-28 14:15:47 -0400

    Add pdf dir path info to multidimensional array

--- resources/config.php
+++ resources/config.php
@@ -28,7 +28,10 @@ $config = array(
             "min" => "/js/main.min.js",
             "html5shiv" => "https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js",
             "respond" => "https://oss.maxcdn.com/respond/1.4.2/respond.min.js"
-        )
+        ),
+        "pdf" => array(
+            "weblearnFlyer" => "/pdf/weblearn-flyer.pdf"
+        ),
     ),
     "jQuery" => array(
         "cdn" => "https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js",
@@ -37,7 +40,6 @@ $config = array(
 );
 
 $gettingStarted = "/getting-started/";
-$weblearnFlyer = "/getting-started/#";
 $mouseSkills = "/getting-started/mouse-skills/";
 $salcStcc = "http://www.stcc.edu/adulteducationcenter/";
 
