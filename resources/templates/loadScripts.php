fbdada341320199785f55dac1797c13aa79bda8f
Andrew S. Gardner <asg@andrewsgardner.com>
2016-06-29 13:31:40 -0400

    Fix code formatting

--- resources/templates/loadScripts.php
+++ resources/templates/loadScripts.php
@@ -1,3 +1,5 @@
-<script src="<?php echo $config['jQuery']['cdn']; ?>"></script><?php echo "\n"; ?>
-<?php echo "\t"; ?><script>window.jQuery || document.write('<script src="<?php echo $config['jQuery']['local']; ?>"><\/script>')</script><?php echo "\n"; ?>
-<?php echo "\t"; ?><script src="<?php echo $config['paths']['scripts']['min'] . $config['info']['cacheVer']; ?>"></script><?php echo "\n"; ?>
+<?php 
+echo "\t<script src=\"" . $config['jQuery']['cdn'] . "\"></script>\n";
+echo "\t<script>window.jQuery || document.write('<script src=\"" . $config['jQuery']['local'] . "\"><\/script>')</script>\n";
+echo "\t<script src=\"" . $config['paths']['scripts']['min'] . $config['info']['cacheVer'] . "\"></script>\n";
+?>
