987b64d9cf1085875bee453b0b00e59efbe7081e
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-05 14:50:31 -0400

    Add Google Analytics tracking code

--- resources/templates/analytics.php
+++ resources/templates/analytics.php
@@ -1,4 +1,4 @@
 <?php 
-echo "\t<script>\n\t\twindow.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;\n\t\tga('create','UA-XXXXX-Y','auto');ga('send','pageview')\n\t</script>\n";
+echo "\t<script>\n\t\twindow.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;\n\t\tga('create','UA-80287082-1','auto');ga('send','pageview')\n\t</script>\n";
 echo "\t<script src=\"https://www.google-analytics.com/analytics.js\" async defer></script>\n";
 ?>
